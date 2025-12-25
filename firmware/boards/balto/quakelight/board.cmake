e2127c66e8dfeb9a02b27e830136cb0c9e563092
Fabio Baltieri <fabio.baltieri@gmail.com>
2025-12-25 02:37:16 +0000

    fw: add quakelight board

---
+++ firmware/boards/balto/quakelight/board.cmake
@@ -0,0 +1,7 @@
+board_runner_args(jlink "--device=STM32H573RI" "--speed=4000")
+board_runner_args(pyocd "--target=stm32h573iikx")
+board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
