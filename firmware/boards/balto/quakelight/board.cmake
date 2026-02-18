d0439c01cc18d46669a0d39d715cb7482831bbf3
Fabio Baltieri <fabio.baltieri@gmail.com>
2026-02-18 20:06:53 +0000

    fw: default to dfu-util

--- firmware/boards/balto/quakelight/board.cmake
+++ firmware/boards/balto/quakelight/board.cmake
@@ -2,6 +2,6 @@ board_runner_args(jlink "--device=STM32H573RI" "--speed=4000")
 board_runner_args(pyocd "--target=stm32h573iikx")
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
